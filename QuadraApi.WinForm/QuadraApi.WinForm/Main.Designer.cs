3fb64c9c70938f59a98d032d3907b52610559a72
SouleDesigns <owen@souledesigns.com>
2015-05-08 12:58:01 -0400

    initial commit to github

---
+++ QuadraApi.WinForm/QuadraApi.WinForm/Main.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace QuadraApi.WinForm
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.loginPanel = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.quadraUrlLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.quadraUrlTextBox = new System.Windows.Forms.TextBox();
+            this.dataPanel = new System.Windows.Forms.Panel();
+            this.itemsButton = new System.Windows.Forms.Button();
+            this.vehiclesButton = new System.Windows.Forms.Button();
+            this.structuresButton = new System.Windows.Forms.Button();
+            this.otherButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.responseTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.dataTypeLabel = new System.Windows.Forms.Label();
+            this.loginPanel.SuspendLayout();
+            this.dataPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginPanel
+            // 
+            this.loginPanel.Controls.Add(this.loginButton);
+            this.loginPanel.Controls.Add(this.passwordLabel);
+            this.loginPanel.Controls.Add(this.userNameLabel);
+            this.loginPanel.Controls.Add(this.quadraUrlLabel);
+            this.loginPanel.Controls.Add(this.passwordTextBox);
+            this.loginPanel.Controls.Add(this.userNameTextBox);
+            this.loginPanel.Controls.Add(this.quadraUrlTextBox);
+            this.loginPanel.Location = new System.Drawing.Point(12, 12);
+            this.loginPanel.Name = "loginPanel";
+            this.loginPanel.Size = new System.Drawing.Size(660, 87);
+            this.loginPanel.TabIndex = 7;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(555, 40);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 13;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(379, 23);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 12;
+            this.passwordLabel.Text = "Password";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(207, 23);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.userNameLabel.TabIndex = 11;
+            this.userNameLabel.Text = "User Name";
+            // 
+            // quadraUrlLabel
+            // 
+            this.quadraUrlLabel.AutoSize = true;
+            this.quadraUrlLabel.Location = new System.Drawing.Point(30, 23);
+            this.quadraUrlLabel.Name = "quadraUrlLabel";
+            this.quadraUrlLabel.Size = new System.Drawing.Size(58, 13);
+            this.quadraUrlLabel.TabIndex = 10;
+            this.quadraUrlLabel.Text = "Quadra Url";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(382, 42);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(151, 20);
+            this.passwordTextBox.TabIndex = 9;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(210, 42);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(145, 20);
+            this.userNameTextBox.TabIndex = 8;
+            // 
+            // quadraUrlTextBox
+            // 
+            this.quadraUrlTextBox.Location = new System.Drawing.Point(30, 42);
+            this.quadraUrlTextBox.Name = "quadraUrlTextBox";
+            this.quadraUrlTextBox.Size = new System.Drawing.Size(155, 20);
+            this.quadraUrlTextBox.TabIndex = 7;
+            this.quadraUrlTextBox.Text = "http://quadradev.aeti-inc.com";
+            // 
+            // dataPanel
+            // 
+            this.dataPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataPanel.Controls.Add(this.dataTypeLabel);
+            this.dataPanel.Controls.Add(this.responseTypeComboBox);
+            this.dataPanel.Controls.Add(this.itemsButton);
+            this.dataPanel.Controls.Add(this.vehiclesButton);
+            this.dataPanel.Controls.Add(this.structuresButton);
+            this.dataPanel.Controls.Add(this.otherButton);
+            this.dataPanel.Controls.Add(this.dataGridView);
+            this.dataPanel.Location = new System.Drawing.Point(0, 0);
+            this.dataPanel.Name = "dataPanel";
+            this.dataPanel.Size = new System.Drawing.Size(692, 483);
+            this.dataPanel.TabIndex = 8;
+            this.dataPanel.Visible = false;
+            // 
+            // itemsButton
+            // 
+            this.itemsButton.Location = new System.Drawing.Point(12, 13);
+            this.itemsButton.Name = "itemsButton";
+            this.itemsButton.Size = new System.Drawing.Size(92, 23);
+            this.itemsButton.TabIndex = 7;
+            this.itemsButton.Text = "Get Items";
+            this.itemsButton.UseVisualStyleBackColor = true;
+            this.itemsButton.Click += new System.EventHandler(this.itemsButton_Click);
+            // 
+            // vehiclesButton
+            // 
+            this.vehiclesButton.Location = new System.Drawing.Point(306, 13);
+            this.vehiclesButton.Name = "vehiclesButton";
+            this.vehiclesButton.Size = new System.Drawing.Size(92, 23);
+            this.vehiclesButton.TabIndex = 5;
+            this.vehiclesButton.Text = "Get Vehicles";
+            this.vehiclesButton.UseVisualStyleBackColor = true;
+            this.vehiclesButton.Click += new System.EventHandler(this.vehiclesButton_Click);
+            // 
+            // structuresButton
+            // 
+            this.structuresButton.Location = new System.Drawing.Point(208, 13);
+            this.structuresButton.Name = "structuresButton";
+            this.structuresButton.Size = new System.Drawing.Size(92, 23);
+            this.structuresButton.TabIndex = 4;
+            this.structuresButton.Text = "Get Structures";
+            this.structuresButton.UseVisualStyleBackColor = true;
+            this.structuresButton.Click += new System.EventHandler(this.structuresButton_Click);
+            // 
+            // otherButton
+            // 
+            this.otherButton.Location = new System.Drawing.Point(110, 13);
+            this.otherButton.Name = "otherButton";
+            this.otherButton.Size = new System.Drawing.Size(92, 23);
+            this.otherButton.TabIndex = 3;
+            this.otherButton.Text = "Get Others";
+            this.otherButton.UseVisualStyleBackColor = true;
+            this.otherButton.Click += new System.EventHandler(this.otherButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(668, 415);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // responseTypeComboBox
+            // 
+            this.responseTypeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.responseTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.responseTypeComboBox.FormattingEnabled = true;
+            this.responseTypeComboBox.Items.AddRange(new object[] {
+            "JSON",
+            "XML"});
+            this.responseTypeComboBox.Location = new System.Drawing.Point(615, 15);
+            this.responseTypeComboBox.Name = "responseTypeComboBox";
+            this.responseTypeComboBox.Size = new System.Drawing.Size(65, 21);
+            this.responseTypeComboBox.TabIndex = 8;
+            // 
+            // dataTypeLabel
+            // 
+            this.dataTypeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataTypeLabel.AutoSize = true;
+            this.dataTypeLabel.Location = new System.Drawing.Point(538, 18);
+            this.dataTypeLabel.Name = "dataTypeLabel";
+            this.dataTypeLabel.Size = new System.Drawing.Size(71, 13);
+            this.dataTypeLabel.TabIndex = 9;
+            this.dataTypeLabel.Text = "Content Type";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 483);
+            this.Controls.Add(this.dataPanel);
+            this.Controls.Add(this.loginPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Quadra API";
+            this.loginPanel.ResumeLayout(false);
+            this.loginPanel.PerformLayout();
+            this.dataPanel.ResumeLayout(false);
+            this.dataPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel loginPanel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label quadraUrlLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox quadraUrlTextBox;
+        private System.Windows.Forms.Panel dataPanel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button vehiclesButton;
+        private System.Windows.Forms.Button structuresButton;
+        private System.Windows.Forms.Button otherButton;
+        private System.Windows.Forms.Button itemsButton;
+        private System.Windows.Forms.ComboBox responseTypeComboBox;
+        private System.Windows.Forms.Label dataTypeLabel;
+
+    }
+}
+
---
+++ QuadraApi.WinForm/QuadraApi.WinForm/Main.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace QuadraApi.WinForm
+{
+    /// <summary>
+    /// Quadra API sample main form
+    /// </summary>
+    public partial class Main : Form
+    {
+
+        #region Private Variables 
+
+        // Aquired at login and passed to subsequent API calls
+        private string _accessToken = string.Empty;
+
+        #endregion
+
+        #region Ctor
+
+        public Main()
+        {
+            InitializeComponent();
+            responseTypeComboBox.SelectedIndex = 0;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            GetAccessToken();
+        }
+
+        private void itemsButton_Click(object sender, EventArgs e)
+        {
+            GetData("/api/items");
+        }
+
+        private void otherButton_Click(object sender, EventArgs e)
+        {
+            GetData("/api/otherexposures");
+        }
+
+        private void structuresButton_Click(object sender, EventArgs e)
+        {
+            GetData("/api/structures");
+        }
+
+        private void vehiclesButton_Click(object sender, EventArgs e)
+        {
+            GetData("/api/vehicles");
+        }
+        
+        #endregion
+
+        #region Private Implementation
+
+        /// <summary>
+        /// Retreive an access token from Quadra API, this token is then passed in subsequent requests for authentication
+        /// </summary>
+        private void GetAccessToken()
+        {
+            var responseText = string.Empty;
+
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+
+                // Format form data - example:  grant_type=password&userName=jdoe@fake.net&password=abc123
+                var pairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>( "grant_type", "password" ), 
+                    new KeyValuePair<string, string>( "userName", userNameTextBox.Text ), 
+                    new KeyValuePair<string, string> ( "password", passwordTextBox.Text )
+                };
+                var content = new FormUrlEncodedContent(pairs);
+
+                // Perform POST on Quadra API
+                using (var client = new HttpClient())
+                {
+                    // Get response
+                    var response = client.PostAsync(quadraUrlTextBox.Text + "/Token", content).Result;
+                    responseText = response.Content.ReadAsStringAsync().Result;                    
+                }
+
+                // Serialize json and get access token
+                var serializer = new JavaScriptSerializer();
+                dynamic authResult = serializer.Deserialize<dynamic>(responseText);
+
+                // Confirm there is a token
+                if (authResult == null || !authResult.ContainsKey("access_token") || authResult["access_token"] == null)
+                    throw new Exception(responseText);
+
+                // Set the token
+                _accessToken = authResult["access_token"].ToString();
+                
+                // Update UI
+                loginPanel.Hide();
+                dataPanel.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+
+          
+        }
+
+        /// <summary>
+        /// Get export data from the input url
+        /// </summary>
+        /// <param name="dataUrl">Relative Quadra API url to retreieve data from</param>
+        /// <param name="responseType">Possible response types include: application/xml application/json </param>
+        private void GetData(string dataUrl)
+        {
+            var responseText = string.Empty;
+            var responseType = string.Empty;
+
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+
+                // Clear display
+                dataGridView.Columns.Clear();
+                dataGridView.Rows.Clear();
+
+                // Get response type based on selected combo value
+                switch(responseTypeComboBox.SelectedItem.ToString())
+                {
+                    case "JSON":
+                        responseType = "application/json";
+                        break;
+                    case "XML":
+                        responseType = "application/xml";
+                        break;
+                }
+
+                
+                // Perform GET on Quadra API
+                using (var c = new HttpClient())
+                {
+                    // Add auth header
+                    c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+
+                    // Specify the response type
+                    c.DefaultRequestHeaders.Add("Accept", responseType);                    
+
+                    // Get response
+                    var response = c.GetAsync(quadraUrlTextBox.Text + dataUrl).Result;
+                    responseText = response.Content.ReadAsStringAsync().Result;                                        
+                }
+
+                // Process response
+                switch (responseType)
+                {
+                    case "application/json":
+                        LoadGridJson(responseText);
+                        break;
+                    case "application/xml":
+                        LoadGridXml(responseText);
+                        break;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+
+        }
+
+        /// <summary>
+        /// Serialize json and populate the results grid
+        /// </summary>
+        /// <param name="json"></param>
+        private void LoadGridJson(string json)
+        {    
+
+            // Serialize json
+            var serializer = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
+            dynamic data = serializer.Deserialize<dynamic>(json);
+
+            // Invalid result parsed as dictionary containing json error
+            if (data is Dictionary<string, object>)
+                throw new Exception(json);
+
+            // Output results
+            if (data != null && data.Length > 0)
+            {
+                // Setup columns
+                Dictionary<string, object> row = data[0];
+                foreach (var column in row.Keys)
+                {
+                    dataGridView.Columns.Add(new DataGridViewColumn()
+                    {
+                        HeaderText = column,                        
+                        CellTemplate = new DataGridViewTextBoxCell(),
+                        ReadOnly = true
+                    });
+                }
+
+                // Add data to grid
+                for (var rowIndex = 0; rowIndex < data.Length; rowIndex++)
+                {
+                    row = data[rowIndex];
+                    dataGridView.Rows.Add(row.Values.ToArray());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read xml into a datatable and populate the results grid
+        /// </summary>
+        /// <param name="xml"></param>
+        private void LoadGridXml(string xml)
+        {
+            var dataTable = new DataTable();
+
+            // Fill data table
+            dataTable.BeginLoadData();
+            using (var reader = new StringReader(xml))            
+                dataTable.ReadXml(reader);            
+            dataTable.EndLoadData();
+                        
+            // Output results
+            if (dataTable != null && dataTable.Rows.Count > 0)
+            {
+                // Setup columns                
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    dataGridView.Columns.Add(new DataGridViewColumn()
+                    {                        
+                        HeaderText = column.Caption,
+                        ValueType = column.DataType,
+                        CellTemplate = new DataGridViewTextBoxCell(),
+                        ReadOnly = true
+                    });
+                }
+
+                // Add data to grid
+                for (var rowIndex = 0; rowIndex < dataTable.Rows.Count; rowIndex++)
+                {
+                    var row = dataTable.Rows[rowIndex];
+                    dataGridView.Rows.Add(row.ItemArray);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
